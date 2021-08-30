4b9c4f04e0a10b806c0ee3e87729047c689dfc53
Tanush Banerjee <samihanbanerjee026@gmail.com>
2021-08-30 22:17:52 +0530

    Added some changes!

--- config.h
+++ config.h
@@ -2,18 +2,22 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */ static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */ static const unsigned int gappih    = 20;       /* horiz inner gap between windows */ static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+
+static const unsigned int snap      = 32;       /* snap pixel */ 
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */ 
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */ 
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=10", "fontawesome:size=10" };
-static const char dmenufont[]       = "JetBrains Mono:size=10";
+static const char *fonts[]          = { "Cascadia Code:size=10", "JoyPixels:size=10" };
+static const char dmenufont[]       = "Cascadia Code:size=10";
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444"; static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray2[]       = "#111111"; static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#9e5f00";
+static const char col_cyan[]        = "#111111";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -76,38 +80,29 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-l", "20", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
+// START_KEYS
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	//{ MODKEY,						XK_w,	   spawn,		   SHCMD("dmenu_run -l 20") },
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,						XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_Delete,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,						XK_z,      incrgaps,       {.i = +3 } },
 	{ MODKEY,						XK_x,      incrgaps,       {.i = -3 } },
-	//{ MODKEY|Mod1Mask,              XK_i,      incrigaps,      {.i = +1 } },
-	//{ MODKEY|Mod1Mask|ShiftMask,    XK_i,      incrigaps,      {.i = -1 } },
-	//{ MODKEY|Mod1Mask,              XK_o,      incrogaps,      {.i = +1 } },
-	//{ MODKEY|Mod1Mask|ShiftMask,    XK_o,      incrogaps,      {.i = -1 } },
-	//{ MODKEY|Mod1Mask,              XK_6,      incrihgaps,     {.i = +1 } },
-	//{ MODKEY|Mod1Mask|ShiftMask,    XK_6,      incrihgaps,     {.i = -1 } },
-	//{ MODKEY|Mod1Mask,              XK_7,      incrivgaps,     {.i = +1 } },
-	//{ MODKEY|Mod1Mask|ShiftMask,    XK_7,      incrivgaps,     {.i = -1 } },
-	//{ MODKEY|Mod1Mask,              XK_8,      incrohgaps,     {.i = +1 } },
-	//{ MODKEY|Mod1Mask|ShiftMask,    XK_8,      incrohgaps,     {.i = -1 } },
-	//{ MODKEY|Mod1Mask,              XK_9,      incrovgaps,     {.i = +1 } },
-	//{ MODKEY|Mod1Mask|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } },
 	{ MODKEY,						XK_a,      togglegaps,     {0} },
 	{ MODKEY|ShiftMask,				XK_a,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
@@ -116,7 +111,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY|ShiftMask,             XK_f,      fullscreen,     {0} },
-	{ MODKEY|ControlMask,		XK_comma,  cyclelayout,    {.i = -1 } },
+	{ MODKEY|ControlMask,			XK_comma,  cyclelayout,    {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
@@ -127,7 +122,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,						XK_w,	   spawn,		   SHCMD("brave") },
+	{ MODKEY,						XK_w,	   spawn,		   SHCMD("librewolf") },
+	{ MODKEY|ShiftMask,				XK_t,	   spawn,		   SHCMD("/home/hacker/fl/scripts/tmux-session") },
 	{ MODKEY|ShiftMask,				XK_w,	   spawn,		   SHCMD("st -e nmtui") },
 	{ MODKEY,						XK_r,	   spawn,		   SHCMD("st -e lf") },
 	{ MODKEY|ShiftMask,				XK_r,	   spawn,		   SHCMD("st -e top") },
@@ -136,18 +132,28 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,				XK_minus,  spawn,		   SHCMD("pulsemixer --change-volume -10") }, 
 	{ MODKEY,						XK_equal,  spawn,		   SHCMD("pulsemixer --change-volume +3") }, 
 	{ MODKEY|ShiftMask,				XK_equal,  spawn,		   SHCMD("pulsemixer --change-volume +10") }, 
-	{ MODKEY,						XK_m,	   spawn,		   SHCMD("st -e ncmpcpp") },
-	{ MODKEY,						XK_p,	   spawn,		   SHCMD("mpc toggle") },
-	{ MODKEY|ShiftMask,				XK_p,				spawn,		SHCMD("mpc pause ; pauseallmpv") },
-	{ MODKEY,						XK_bracketleft,		spawn,		SHCMD("mpc seek -10") },
-	{ MODKEY|ShiftMask,				XK_bracketleft,		spawn,		SHCMD("mpc seek -60") },
-	{ MODKEY,						XK_bracketright,	spawn,		SHCMD("mpc seek +10") },
-	{ MODKEY|ShiftMask,				XK_bracketright,	spawn,		SHCMD("mpc seek +60") },
-	{ MODKEY,			XK_comma,	spawn,		SHCMD("mpc prev") },
-	{ MODKEY|ShiftMask,		XK_comma,	spawn,		SHCMD("mpc seek 0%") },
-	{ MODKEY,			XK_period,	spawn,		SHCMD("mpc next") },
-	{ MODKEY|ShiftMask,		XK_period,	spawn,		SHCMD("mpc repeat") },
+	{ MODKEY|Mod1Mask,				XK_v,	   spawn,		   SHCMD("/home/hacker/fl/scripts/vidplay") },
+	{ MODKEY,						XK_BackSpace,   spawn,		   SHCMD("/home/hacker/fl/scripts/shutpc") },
+	{ MODKEY|ControlMask,			XK_w,			spawn,		   SHCMD("/home/hacker/fl/scripts/cg.sh") },
+	{ MODKEY|Mod1Mask,			XK_p,			spawn,		   SHCMD("st -e python") },
 
+
+	// Music stuff
+	{ MODKEY|ShiftMask,				XK_m,				spawn,			SHCMD("st -e ncmpcpp") },
+	{ MODKEY,						XK_p,				spawn,			SHCMD("mpc toggle") },
+	{ MODKEY|ControlMask,			XK_p,				spawn,			SHCMD("playerctl play-pause") },
+	{ MODKEY|ShiftMask,				XK_p,				spawn,			SHCMD("mpc pause ; pauseallmpv") },
+	{ MODKEY,						XK_bracketleft,		spawn,			SHCMD("mpc seek -10") },
+	{ MODKEY|ShiftMask,				XK_bracketleft,		spawn,			SHCMD("mpc seek -60") },
+	{ MODKEY,						XK_bracketright,	spawn,			SHCMD("mpc seek +10") },
+	{ MODKEY|ShiftMask,				XK_bracketright,	spawn,			SHCMD("mpc seek +60") },
+	{ MODKEY,						XK_comma,			spawn,			SHCMD("mpc prev") },
+	{ MODKEY|ShiftMask,				XK_comma,			spawn,			SHCMD("mpc seek 0%") },
+	{ MODKEY,						XK_period,			spawn,			SHCMD("mpc next") },
+	{ MODKEY|ShiftMask,				XK_period,			spawn,			SHCMD("mpc repeat") },
+	{ 0,							XK_Print,			spawn,			SHCMD("maim full-$(date '+%y%m%d-%H%M-%S').png") },
+	{ ShiftMask,					XK_Print,			spawn,			SHCMD("/home/hacker/fl/scripts/dmenurecshot") },
+	{ MODKEY|ControlMask,			XK_Print,			spawn,			SHCMD("/home/hacker/fl/scripts/dmenurecord") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -160,6 +166,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
+// END_KEYS
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
@@ -176,4 +183,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
